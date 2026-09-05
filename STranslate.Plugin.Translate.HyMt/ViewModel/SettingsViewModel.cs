58a775854c50261d46dc4bed9032bd5da925a54b
Felix <felix@example.com>
2026-09-05 14:50:32 +0800

    fix: import JsonNode namespace

--- STranslate.Plugin.Translate.HyMt/ViewModel/SettingsViewModel.cs
+++ STranslate.Plugin.Translate.HyMt/ViewModel/SettingsViewModel.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Globalization;
 using System.Security.Cryptography;
 
